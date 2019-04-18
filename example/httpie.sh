b599c76616493115d372dfa6aa8ec0f7ad6aeeef
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-04-18 15:17:13 +0200

    enhanced httpie script: higher resolutions

--- example/httpie.sh
+++ example/httpie.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-http -f POST localhost:8888/v1/generatemultiple meta="{ \"page\": 1, \"resolutions\": [ { \"width\": 100, \"height\": 141 }, { \"width\": 200, \"height\": 242 } ] }" file@document.pdf > response.json && \
+http -f POST localhost:8888/v1/generatemultiple meta="{ \"page\": 1, \"resolutions\": [ { \"width\": 200, \"height\": 283}, { \"width\": 300, \"height\": 378} ] }" file@document.pdf > response.json && \
     rm -f *.jpg && \
     jq -r '.base64Images[0]' response.json | base64 -d > 1.jpg && \
     jq -r '.base64Images[1]' response.json | base64 -d > 2.jpg && \
