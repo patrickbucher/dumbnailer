901b1e2cada4cafeb475731676ad2c1804a41e94
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-04-18 09:49:08 +0200

    sort resolution parameters in descending order so that ImageMagick uses bigger images to create smaller ones, not the other way around

--- params.go
+++ params.go
@@ -4,23 +4,38 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sort"
 )
 
 type Meta struct {
-	Page        int          `json:"page"`
-	Resolutions []Resolution `json:"resolutions"`
+	Page        int        `json:"page"`
+	Resolutions Dimensions `json:"resolutions"`
 }
+
+type Dimensions []Resolution
 
 type Resolution struct {
 	Width  int `json:"width"`
 	Height int `json:"height"`
 }
 
+func (r Resolution) Size() int {
+	return r.Width * r.Height
+}
+
+func (d Dimensions) Len() int           { return len(d) }
+func (d Dimensions) Swap(i, j int)      { d[i], d[j] = d[j], d[i] }
+func (d Dimensions) Less(i, j int) bool { return d[i].Size() < d[j].Size() }
+
 func (m *Meta) prepareCommand(pdfFileName string) ([]string, []*os.File, error) {
 	var args []string
 	var files []*os.File
 	args = append(args, fmt.Sprintf("%s[%d]", pdfFileName, m.Page-1))
 	args = append(args, "-flatten")
+
+	// convert from original to subsequent smaller resolutions
+	sort.Sort(sort.Reverse(m.Resolutions))
+
 	for i, res := range m.Resolutions {
 		args = append(args, "-thumbnail")
 		args = append(args, fmt.Sprintf("%dx%d!", res.Width, res.Height))
